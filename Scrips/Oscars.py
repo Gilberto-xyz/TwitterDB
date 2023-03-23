abef012e06054a1cb9ae5ef945d1d6bbe5dae881
Gilberto.xyz <gilberto.nava.marcos@outlook.com>
2023-03-23 03:26:50 -0600

    data

---
+++ Scrips/Oscars.py
@@ -0,0 +1,30 @@
+# Obtener twits mediante snscrape
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+from textblob import TextBlob
+
+# Definir parámetros de búsqueda
+consulta = "(oscars, OR oscar) lang:en"
+datos = []
+limite = 50_000
+
+# Obtener twits
+for tweet in sntwitter.TwitterSearchScraper(consulta).get_items():
+    
+    if len(datos) == limite:
+        break
+    else:
+        # Obtener sentimiento y polaridad
+        blob = TextBlob(tweet.rawContent)
+        polarity = blob.sentiment.polarity
+        subjectivity = blob.sentiment.subjectivity
+
+        # imprimir el numero de twits obtenidos hasta el momento
+        print(len(datos), end='\r')
+        datos.append([tweet.date, tweet.user.username, tweet.rawContent, polarity, subjectivity])
+
+# Crear dataframe
+df = pd.DataFrame(datos, columns=['Date', 'User', 'Tweet', 'Polarity', 'Subjectivity'])
+
+# Exportar a csv
+df.to_csv('Oscars.csv', index=False)
---
+++ Subir_tw_otan.py
@@ -0,0 +1,27 @@
+# Subir datos de un archivo csv a una base de datos en MySQL
+
+import mysql.connector
+import pandas as pd
+
+# Conexion a la base de datos MySQL
+connect_mydb = mysql.connector.connect(host='localhost',
+                               user='root',
+                               database='twitter_db',
+                               port='3306')
+# Leer el archivo csv con los tweets del primer dataset
+dataframe = pd.read_csv('Otan.csv')
+
+# Crear un cursor para ejecutar las consultas
+cursor = connect_mydb.cursor()
+
+# Crear una tabla en la base de datos MySQL donde se almacenaran los tweets del primer dataset 
+cursor.execute('CREATE TABLE tweets_otan (id INT AUTO_INCREMENT PRIMARY KEY, Date VARCHAR(255), Username VARCHAR(255), Tweet VARCHAR(255), Polarity VARCHAR(255), Subjectivity VARCHAR(255))')
+
+# Insertar valores del dataframe en la tabla creada en la base de datos MySQL 
+for i, row in dataframe.iterrows():
+    sql = "INSERT INTO tweets_otan (Date, Username, Tweet, Polarity, Subjectivity) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, tuple(row))
+    # Imprimir el numero de filas insertadas hasta el momento
+    print("Filas insertadas: ",i, end='\r')
+    connect_mydb.commit()
+
